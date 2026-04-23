750b49a716b2620b0e183662db5e93f55f854dce
not_malware <138697993+not-malware-exe@users.noreply.github.com>
2026-04-23 11:06:54 -0600

    JS Variables and Constants

---
+++ ponder/js_variables_and_constants/week1.js
@@ -0,0 +1,31 @@
+
+// javascript land but it is its own file
+
+/*
+    Cheese
+    Cheese
+    Cheese
+*/
+
+// let is the keyword for variables
+let name = "Ur Mom";
+let age = 74930285579385;
+
+age = age + 1
+age += 1;
+age++; // it work, wow
+
+// print for js
+console.log(age);
+
+// constant, woah
+const CHICKEN_TENDERS = "Chicken Tenders"
+// CHICKEN_TENDERS = "Chicken Nuggets" // error
+
+
+//scope, same as any other language
+if (name === "Ur Mom"){ // == checks value, === checks value and type
+    let pounds = 999999999
+    console.log("Ur mom weighs " + String(pounds) + " lbs.")
+}
+// console.log("Ur mom weighs " + String(pounds) + " lbs.") // error
