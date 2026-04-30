51981589c86e89ca022b15842a9ec681fba4f486
not_malware <138697993+not-malware-exe@users.noreply.github.com>
2026-04-30 11:02:07 -0600

    cheese

--- ponder/dom-theme/dom-theme.js
+++ ponder/dom-theme/dom-theme.js
@@ -14,6 +14,9 @@ function changeTheme() {
     } else if (current === 'desert') {
         document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/desert.jpg')";
         pageContent.style.fontFamily = "'Big Caslon', serif";
+    } else if (current === '???') {
+        document.body.style.backgroundImage = "url('https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Parmigiano_reggiano_factory.jpg/500px-Parmigiano_reggiano_factory.jpg')";
+        pageContent.style.fontFamily = "'Impact', sans-serif";
     } else {
         // default
         document.body.style.backgroundImage = "none";
