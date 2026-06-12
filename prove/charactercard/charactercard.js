de2f5e72dd397df80e2b14abdfb227581e6ef698
not_malware <138697993+not-malware-exe@users.noreply.github.com>
2026-06-11 22:27:48 -0600

    Did character card assignment correct this time.

--- prove/charactercard/charactercard.js
+++ prove/charactercard/charactercard.js
@@ -1,27 +1,56 @@
+const characterObj = {
+    name : "Duck The Wise",
+    image : "images/DuckOfWisdomBig.png",
+    race : "Duck",
+    class : "Warlock",
+    hp : 2,
+    level : 999,
+    updateCard : function() {
+        document.querySelector(".name").textContent = this.name;
+        document.querySelector(".image").src = this.image;
 
+        document.querySelector(".race_field").innerHTML =  `<b>Race:</b> ${this.race}`;
+        document.querySelector(".class_field").innerHTML =  `<b>Class:</b> ${this.class}`;
+        document.querySelector(".hitpoints_field").innerHTML =  `<b>Hitpoints:</b> ${this.hp}`;
+        document.querySelector(".level_field").innerHTML =  `<b>Level:</b> ${this.level}`;
+    },
+    takeDamage : function(amount = 1, displayDeath = true){
+        if (this.hp === 0){
+            return;
+        }
+        
+        this.hp -= amount;
+        if (this.hp < 0){
+            this.hp = 0;
+        }
 
+        this.updateCard();
 
-let hp = 2;
-let level = 999;
+        if (this.hp === 0 && displayDeath){
+            setTimeout(() => {window.alert(`${this.name} died.`)},500);
+        }
+    },
+    increaseLevel : function(amount = 1){
+        this.level += amount
+        this.updateCard();
+    },
+    setImage : function(image = ""){
+        this.image = image;
+        this.updateCard();
+    }
+}
 
-const duckImage = document.getElementById("duck_img");
+characterObj.updateCard();
 
-const hitpointsField = document.getElementById("hitpoints_field");
-const levelField = document.getElementById("level_field");
-
-const attackButton = document.getElementById("attack_button");
-const levelUpButton = document.getElementById("level_up_button");
+const attackButton = document.querySelector(".attack_button");
+const levelUpButton = document.querySelector(".level_up_button");
 
 attackButton.addEventListener("click",event => {
-    if (hp === 0){
-        return;
-    }
-        
-    hp--;
-    hitpointsField.innerHTML =`<b>Hitpoints:</b> ${hp}`;
+    characterObj.takeDamage(1,false);
 
-    if (hp === 0){
-        duckImage.src = "images/DuckOfWisdomEvil.png";
+    if (characterObj.hp === 0){
+        characterObj.setImage("images/DuckOfWisdomEvil.png");
+
         setTimeout(() => {
             window.alert("You provoked the Duck.");
             window.alert("Don't worry, it will be all over soon.");
@@ -33,12 +62,11 @@ attackButton.addEventListener("click",event => {
 })
 
 levelUpButton.addEventListener("click",event => {
-    level++;
-    levelField.innerHTML = `<b>Level:</b> ${level}`;
+    characterObj.increaseLevel(1);
 
-    duckImage.src = "images/DuckOfWisdomGood.png";
+    characterObj.setImage("images/DuckOfWisdomGood.png");
 
     setTimeout(() => {
-        duckImage.src = "images/DuckOfWisdomBig.png"
+        characterObj.setImage("images/DuckOfWisdomBig.png");
     },500);
 })
